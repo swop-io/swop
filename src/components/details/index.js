3d00bcd922d6f11fee8177b38b3976a6369b5c77
karlptrck <camota.kp@gmail.com>
2019-08-03 21:36:34 -0400

    added bidding layout

--- src/components/details/index.js
+++ src/components/details/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import 'bulma'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import Footer from '../footer'
 
 class TicketDetails extends React.Component {
     render() {
@@ -73,8 +74,76 @@ class TicketDetails extends React.Component {
 
                         <p class="title is-5">Bids</p>
                         <div class="box">
-                          
-                            <p class="subtitle">This column will take up the remaining space available.</p>
+                        <div class="level">
+                                    <div class="level-item has-text-centered">
+                                        <div>
+                                        <p class="heading">Lowest Ask</p>
+                                        <p class="title">$300</p>
+                                        <p>1.01 ETH</p>
+                                        </div>
+                                    </div>
+
+                                    <div class="level-item has-text-centered">
+                                        <div>
+                                        <p class="heading">Highest Bid</p>
+                                        <p class="title">$350</p>
+                                        <p>1.01 ETH</p>
+                                        </div>
+                                    
+                                    </div>
+                                    <div class="level-item">
+                                        <div>
+                                        
+                                       
+                                        <div class="control">
+                                        <input class="input is-small" style={{width : 100}} type="text" placeholder="Amount in USD"></input>
+                                       
+                                        </div>
+                                        <button class="button is-black is-small is-pulled-right">Place Bid</button>
+                                        </div>
+                                    </div>
+                                </div>
+
+
+                        <p>Bid History</p>
+                        <table class="table is-fullwidth">
+                            <thead>
+                                <tr>
+                                <th>User</th>
+                                <th>Datetime</th>
+                                <th>Amount (USD)</th>
+                                </tr>
+                            </thead>
+
+                            <tbody>
+                                <tr>
+
+                                <td>0xE5CDaa796A8A...</td>
+                                <td>08/03/2019 - 20:00:00</td>
+                                <td>200</td>
+                                </tr>
+                                <tr>
+                                
+                                <td>0xE5CDaa796A8A...</td>
+                                <td>08/03/2019 - 20:00:00</td>
+                                <td>180</td>
+                                </tr>
+                                <tr>
+                                
+                                    <td>0xE5CDaa796A8A...</td>
+                                <td>08/03/2019 - 20:00:00</td>
+                                <td>150</td>
+                                </tr>
+                                <tr>
+                                
+                                <td>0xE5CDaa796A8A...</td>
+                                <td>08/03/2019 - 20:00:00</td>
+                                <td>100</td>
+                                </tr>
+
+
+                            </tbody>
+                        </table>
                         </div>
                     </div>
                     
@@ -95,6 +164,7 @@ class TicketDetails extends React.Component {
                 </div>
 
 
+               
             </div>
         )
     }
