721779422493a6627b97c20f5fe1f256a2561615
karlptrck <camota.kp@gmail.com>
2019-08-16 15:30:38 -0400

    Updates

---
+++ precache-manifest.fae646e7307e1f6773c0018e39060311.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39784f046f90d8db412cfbca7c039928",
+    "url": "/swop-ui/index.html"
+  },
+  {
+    "revision": "3a9ca4afbda315ac3572",
+    "url": "/swop-ui/static/css/2.9ad73318.chunk.css"
+  },
+  {
+    "revision": "e9ca01c421d6c67b42e6",
+    "url": "/swop-ui/static/css/main.314a2348.chunk.css"
+  },
+  {
+    "revision": "3a9ca4afbda315ac3572",
+    "url": "/swop-ui/static/js/2.3a0206ad.chunk.js"
+  },
+  {
+    "revision": "e9ca01c421d6c67b42e6",
+    "url": "/swop-ui/static/js/main.87f12bb3.chunk.js"
+  },
+  {
+    "revision": "3512237febab13f54dab",
+    "url": "/swop-ui/static/js/runtime~main.951136ad.js"
+  },
+  {
+    "revision": "3328e0b74fd1bd1eacf15f491dff9491",
+    "url": "/swop-ui/static/media/2.3328e0b7.png"
+  },
+  {
+    "revision": "d35c7bf9bd9807309b1edd6fdf3cd1da",
+    "url": "/swop-ui/static/media/3.d35c7bf9.png"
+  },
+  {
+    "revision": "4196c7887902aee4f6c10f04b69777c4",
+    "url": "/swop-ui/static/media/4.4196c788.png"
+  },
+  {
+    "revision": "c2f9344f099ec107a1ba0b21b1cf15f4",
+    "url": "/swop-ui/static/media/5.c2f9344f.png"
+  },
+  {
+    "revision": "af6bcd2e1206c6721602ec92bb36c604",
+    "url": "/swop-ui/static/media/air_canada.af6bcd2e.png"
+  },
+  {
+    "revision": "18e4ebbf3ade4b252434daa3e100969b",
+    "url": "/swop-ui/static/media/banner.18e4ebbf.png"
+  },
+  {
+    "revision": "729c121bd01e27ef5b666364ec7a2157",
+    "url": "/swop-ui/static/media/delta.729c121b.jpg"
+  },
+  {
+    "revision": "413abe0d020f52e86b2ea338a92a5f5f",
+    "url": "/swop-ui/static/media/swop-logo.413abe0d.svg"
+  },
+  {
+    "revision": "f05cae48a7763ae9328496928f5fc007",
+    "url": "/swop-ui/static/media/westjet.f05cae48.jpg"
+  }
+]);
